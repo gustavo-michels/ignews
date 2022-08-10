1bcfc61e1764ee92797f4deac9adca5c1b0d5e10
Gustavo H. Michels <ghmichels@gmail.com>
2022-08-10 16:58:36 -0300

    added stripe integration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
-import type { NextPage } from 'next'
+import type { GetStaticProps, NextPage } from 'next'
 import Head from 'next/head'
+import { SubscribeButton } from '../components/SubscribeButton'
+import { stripe } from '../services/stripe'
 
 import styles from './home.module.scss'
 
-const Home: NextPage = () => {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+const Home: NextPage = ({ product }: HomeProps) => {
   return (
     <>
       <Head>
@@ -15,13 +24,34 @@ const Home: NextPage = () => {
           <h1>News about the <span>React</span> world.</h1>
           <p>
             Get access to all the plubications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   )
+}
+
+export const getStaticProps:GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1LVK4xEy0HGQAL0nqKoCTMVK', {
+    expand: ['product']
+  })
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  }
+  return {
+    props: {
+      product
+    },
+    revalidate: 60 * 60 * 24 // 24hours
+  }
 }
 
 export default Home
